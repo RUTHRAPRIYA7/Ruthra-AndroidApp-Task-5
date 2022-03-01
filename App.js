0a7de7075827f90476b249c8b1719edac177b429
RUTHRAPRIYA <98040179+RUTHRAPRIYA7@users.noreply.github.com>
2022-03-01 22:04:34 +0530

    Add files via upload

---
+++ App.js
@@ -0,0 +1,38 @@
+import React, { useState } from 'react';
+import { StyleSheet, Text, View, StatusBar, Button } from 'react-native';
+import CountContextProvider from './src/countcont.js';
+import { useCountContext } from './src/countcont.js';
+import Mainscreen from './src/mainscreen';
+
+export default function App() {
+  
+  const CountContext= useCountContext();
+  console.log(CountContext);
+
+  return (
+    
+    <CountContextProvider>
+    <Mainscreen>
+
+    </Mainscreen>
+    </CountContextProvider>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    marginTop:10,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  welcome: {       
+    backgroundColor:"green"
+  },
+  bg: {
+    marginTop:10,
+    backgroundColor: '#00FF00',
+    padding:10
+
+  }
+});
